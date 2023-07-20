485f956253079c6ede1c3f8d2d098f9828865724
Rob Barnsley <R.Barnsley@skatelescope.org>
2023-07-20 16:23:05 +0100

    amended datalink response for soda

--- rucio-datalink/src/rucio_datalink/rest/server.py
+++ rucio-datalink/src/rucio_datalink/rest/server.py
@@ -13,7 +13,7 @@ import geoip2.database
 from geopy.distance import great_circle
 import liboidcagent as agent
 from starlette.config import Config
-from starlette.responses import Response, JSONResponse
+from starlette.responses import Response, HTMLResponse, JSONResponse
 
 config = Config()
 
@@ -38,16 +38,22 @@ geoip_reader = None
 if config.get("GEOIP_LICENSE_KEY", default=None):
     geoip_reader = geoip2.database.Reader(config.get('GEOIP_DATABASE_PATH'))
 
-# Query sited for list of storage lat/longs #FIXME: needs to update periodically
+# FIXME: needs to update periodically, but don't want to hammer the api
+# Query sited for list of storage lat/longs and services
 #
-sites = None
-if config.get("SITED_STORAGES_ENDPOINT_JSON", default=None):
-    response = requests.get(config.get('SITED_STORAGES_ENDPOINT_JSON'))
+rses = None
+services = None
+if config.get("SITED_STORAGES_ENDPOINT", default=None):
+    response = requests.get("{}/storages/grafana".format(config.get('SITED_STORAGES_ENDPOINT')))
     content = response.content.decode('utf-8')
-    sites = json.loads(content)
+    rses = json.loads(content)
+
+    response = requests.get("{}/services".format(config.get('SITED_STORAGES_ENDPOINT')))
+    content = response.content.decode('utf-8')
+    services_by_site = json.loads(content)
 
 # Dependency to refresh access token if necessary.
-async def refresh_access_token() -> Union[str, HTTPException]:
+def refresh_access_token() -> Union[str, HTTPException]:
     try:
         token = agent.get_access_token(config.get('OIDC_CLIENT_NAME'))
     except agent.OidcAgentError as e:
@@ -60,15 +66,21 @@ async def ping(request: Request):
     return JSONResponse('pong')
 
 
-@app.get('/links', response_class=templates.TemplateResponse)
-async def links(id, request: Request, client_ip_address: str = None, sort: str = 'nearest_by_client',
+@app.get('/links', response_class=HTMLResponse)
+async def links(id, request: Request, client_ip_address: str = None, sort: str = 'nearest_by_client', must_include_soda: bool = False,
                 ranking: int = 0, token=Depends(refresh_access_token)) -> Union[templates.TemplateResponse, HTTPException]:
-    scope, name = id.split(':')
+    try:
+        scope, name = id.split(':')
+    except ValueError:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, 'could not parse id.')
+
+    # Get replicas for this DID.
+    #
     parameters = {
         'dids': [{
             'scope': scope,
             'name': name
-        }]
+        }],
     }
     response = requests.post(urllib.parse.urljoin(config.get('RUCIO_CFG_HOST'), 'replicas/list'),
         headers={
@@ -78,41 +90,108 @@ async def links(id, request: Request, client_ip_address: str = None, sort: str =
         data=json.dumps(parameters).encode('utf-8')
     )
     content = response.content.decode('utf-8')
-
     if response.status_code != 200:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Error getting replica data {}".format(content))
-
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "error getting replica data {}".format(content))
     if content:
         replicas_by_rse = json.loads(content)['rses']
     else:
         raise HTTPException(status.HTTP_204_NO_CONTENT)
 
-    if sort == 'random':
-        selection = random.choice(list(replicas_by_rse.values()))[0]
-    elif sort == 'nearest_by_client':
-        if not geoip_reader or not sites:
-            raise HTTPException(status.HTTP_400_BAD_REQUEST, "nearest_by_client not supported.")
+    # Get metadata for this DID.
+    #
+    params = {
+        'plugin': 'POSTGRES_JSON'
+    }
+    response = requests.get(
+        urllib.parse.urljoin(config.get('RUCIO_CFG_HOST'), 'dids/{}/{}/meta'.format(
+            scope, name
+        )),
+        headers={
+            'X-Rucio-Auth-Token': token,
+            'Content-type': 'application/json'
+        },
+        params=params
+        )
+    content = response.content.decode('utf-8')
+    if response.status_code != 200:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "error getting metadata {}".format(content))
+    metadata = json.loads(content)
 
+    # Refine replicas based on SODA service availability, if requested.
+    #
+    soda_services_by_rse = OrderedDict()
+    if must_include_soda:
+        # Get a list of SODA services per RSE.
+        #
+        # FIXME: yikes
+        if not services_by_site:
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "must_include_soda not supported.")
+        for rse, replicas in replicas_by_rse.items():
+            for site in services_by_site:
+                for service in site['services']:
+                    if service['type'] == 'Rucio Storage Element (RSE)' and \
+                            service['identifier'] == rse:
+                        for _service in site['services']:
+                            if _service['type'] == 'SODA':
+                                if not soda_services_by_rse.get(rse, None):
+                                    soda_services_by_rse[rse] = []
+                                soda_services_by_rse[rse].append(_service)
+        for site in set(replicas_by_rse.keys()) - set(soda_services_by_rse.keys()):
+            replicas_by_rse.pop(site)
+
+    if not replicas_by_rse:
+        if must_include_soda:
+            details = "No replicas with local SODA service found for this DID."
+        else:
+            details = "No replicas found for this DID."
+        raise HTTPException(status.HTTP_404_NOT_FOUND, details)
+
+    # Get a replica depending on the sort algorithm.
+    #
+    selected_rse = None
+    selected_rse_replica = None
+    if sort == 'random':
+        selected_rse = random.choice(list(replicas_by_rse.keys()))
+        selected_rse_replica = random.choice(replicas_by_rse[selected_rse])
+    elif sort == 'nearest_by_client':
+        if not geoip_reader or not rses:
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "nearest_by_client not supported.")
         if not client_ip_address:
             client_ip_address = request.client.host
-
         try:
             response = geoip_reader.city(client_ip_address)
             client_location = (response.location.latitude, response.location.longitude)
-            distances_by_site = OrderedDict()
-            for site in sites:
-                if site['name'] in replicas_by_rse:
-                    site_location = (site['latitude'], site['longitude'])
-                    distances_by_site[site['name']] = int(great_circle(client_location, site_location).km)
-            access_url = replicas_by_rse[sorted(distances_by_site, key=distances_by_site.get)[ranking]][0]
+            distances_by_rse = OrderedDict()
+            for rse in rses:
+                if rse['name'] in replicas_by_rse:
+                    rse_location = (rse['latitude'], rse['longitude'])
+                    distances_by_rse[rse['name']] = int(great_circle(client_location, rse_location).km)
+            selected_rse = sorted(distances_by_rse, key=distances_by_rse.get)[ranking]
+            selected_rse_replica = random.choice(replicas_by_rse[selected_rse])
         except geoip2.errors.AddressNotFoundError:
-            logging.warning("address not found, falling back to random")
-            access_url = random.choice(list(replicas_by_rse.values()))
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "client ip addressed could not be geolocated.")
+    else:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "sort algorithm not understood.")
 
-        return templates.TemplateResponse("datalink.xml", {
-            "request": request,
-            "did": id,
-            "access_url": access_url,
-            "ivoa_authority": config.get('IVOA_AUTHORITY')
-        })
+    access_url = selected_rse_replica
+    soda_service = {}
+    if must_include_soda:
+        soda_service = random.choice(soda_services_by_rse[selected_rse])
+    return templates.TemplateResponse("datalink.template.xml", {
+        "request": request,
+        "ivoa_authority": config.get('IVOA_AUTHORITY'),
+        "path_on_storage": "{}/{}".format(scope, access_url.split(scope)[1].lstrip('/')),       #FIXME: this doesn't necessarily work for non-deterministic, need to look it up
+        "access_url": access_url,
+        "description": metadata.get('obs_id', ''),
+        "content_type": metadata.get('content_type', ''),
+        "content_length": metadata.get('content_length', ''),
+        "include_soda": must_include_soda,
+        "soda_resource_identifier": soda_service.get('other_attributes', {}).get('resourceIdentifier', {}).get('value', None) or '',    # e.g. {"resourceIdentifier": {"value": "ivo://skao.src/spsrc-soda/"}}
+        "soda_access_url": "{}://{}:{}/{}".format(
+            soda_service.get('prefix', ''),
+            soda_service.get('host', ''),
+            soda_service.get('port', ''),
+            soda_service.get('path', '').lstrip('/')
+        )
+    }, media_type="application/xml")
 
