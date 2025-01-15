9de74109b3669c671f8fed1cd0f7436d2e30f6dc
rob barnsley <rob.barnsley@skao.int>
2025-01-15 16:49:35 +0000

    refactoring charts

--- rucio-datalink/etc/docker/init.sh
+++ rucio-datalink/etc/docker/init.sh
@@ -5,5 +5,5 @@ export SERVICE_VERSION=`cat VERSION`
 cd /opt/rucio_datalink/src/rucio_datalink/rest
 
 # run server in bg
-uvicorn server:app --host "0.0.0.0" --port $SERVICE_DATALINK_PORT --reload --reload-dir ../../../etc/ --reload-include *.xml
+uvicorn server:app --host "0.0.0.0" --port ${SERVICE_PORT:-$SERVICE_DATALINK_PORT} --reload --reload-dir ../../../etc/ --reload-include *.xml
 
